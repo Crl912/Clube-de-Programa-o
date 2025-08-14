bef15e0b65fa4b37882315158fe8b5baf208be9a
Crl912 <caio.lins@loyola.g12.br>
2025-08-14 15:00:08 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,22 @@
+print('Hello World')
+
+nome = 'Caio'
+idade = '13'
+turma = 'A'
+
+print('O meu nome é ' + nome)
+print('eu tenho ' + str(idade) + ' anos ')
+print('minha turma é ' + turma)
+print()
+print()
+
+num1 = 10
+num2 = 5
+num3 = 5
+num4 = 10
+
+
+resultado = num1 + num2 + num3 + num4 
+print(resultado)
+resultado2 = resultado / 4
+print(resultado2)
